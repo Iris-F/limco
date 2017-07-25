a9f543189c801b1c6faa3114220a5fbe1c213092
Iris <IrisFehse@gmail.com>
2017-07-25 12:18:08 +0200

    Created Guest Apartment website as subpage of main page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Added following code as instructed by Treehouse Tutorial
   get "/pages/:page" => "pages#show"
-  root "pages#show", page: "welcome"
+  root "pages#show", page: "home"
   #Rails Girls Tutorial puts this code here: root to: redirect('/welcome')
 end
