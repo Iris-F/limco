efa07628bbe6f8e9e6208d126757bb6f8f350c70
Iris <IrisFehse@gmail.com>
2017-07-27 18:05:01 +0200

    Second release

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,19 +1,27 @@
 class RequestsController < ApplicationController
 
   def index
+    @requests = Request.all
   end
+
+
 
   def create
     @request = Request.new(request_params)
 
-    @request.save
-    redirect_to @request
+    if @request.save
+      redirect_to @request
+    else
+      render 'new'
+    end
   end
 
   def new
+    @request = Request.new
   end
 
   def edit
+    @request = Request.find(params[:id])
   end
 
   def show
@@ -21,6 +29,13 @@ class RequestsController < ApplicationController
   end
 
   def update
+    @request = Request.find(params[:id])
+
+    if @request.update(request_params)
+      redirect_to @request
+    else
+      render 'edit'
+    end
   end
 
   private
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,8 @@
 class Request < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :startdate, presence: true
+  validates :enddate, presence: true
+  validates :noofguests, presence: true
+#  validates_inclusion_of :email, :in => "@"
 end
