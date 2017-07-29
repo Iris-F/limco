65926f0e97b575456dfc4daa1d35a53c03240346
Iris Fehse <IrisFehse@hotmail.com>
2017-07-29 17:38:53 +0200

    Added the guestbook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 # The following code says a user needs to be logged in to do anything
-#  before_action :authenticate_user!
+#  before_action :authenticate_user!, except: [:welcome]
 end
